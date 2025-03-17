f2cb3978b659ade307b12545a5e6888deb365a9a
manamjong <nskynet5374@naver.com>
2025-03-17 12:44:07 +0900

    빈

---
+++ jsp_250312/src/main/java/hello/HelloBean.java
@@ -0,0 +1,24 @@
+package hello;
+
+public class HelloBean {
+	private String name="hong gill dong";
+	private int age = 20;
+	
+	public int getAge() {
+		return age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
